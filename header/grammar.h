071ae51f73efc943d1cc541fac1fd547aa38ab12
Lucas Dantas <lucasmedeiros@trt21.jus.br>
2025-12-05 15:37:57 -0300

    fix: fix in the grammar function

--- header/grammar.h
+++ header/grammar.h
@@ -6,30 +6,28 @@
 #include <map>
 #include <vector>
 #include <string>
-#include <algorithm>
-
 using namespace std;
 
 class Grammar{
-    private:
-        set<string> terminals;
-        set<string> variables;
-        string startSymbol;
-        map<string, set<vector<string>>> productions;
-    public:
-        Grammar();
-        Grammar(string startSymbol, set<string> terminals);
+private:
+    set<string> terminals;
+    set<string> variables;
+    string startSymbol;
+    map<string, set<vector<string>>> productions;
+public:
+    Grammar();
+    Grammar(string startSymbol, set<string> terminals);
 
-        void addProduction(string A, vector<string> rhs);
-        void removeProduction(string A, vector<string> rhs);
-        void print(ostream &out);
-        Grammar clone() const;
+    void addProduction(string A, vector<string> rhs);
+    void removeProduction(string A, vector<string> rhs);
+    void print(ostream &out);
+    Grammar clone() const;
 
-        set<vector<string>> getProductions(string A);
-        string getStartSymbol(){return startSymbol;}
-        set<string> getVariables(){return variables;}
+    set<vector<string>> getProductions(string A);
+    string getStartSymbol(){return startSymbol;}
+    set<string> getVariables(){return variables;}
 
-        void setStartSymbol(string A){this->startSymbol = A;}
+    void setStartSymbol(string A){this->startSymbol = A;}
 };
 
 #endif
