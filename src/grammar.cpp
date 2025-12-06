0df496ee1fa2e286bd8a80b617a238f5e2a5512a
Lucas Dantas <lucasvdmedeiros@proton.me>
2025-12-05 22:42:15 -0300

    feat: implementation of the GrammarParser fenction

--- src/grammar.cpp
+++ src/grammar.cpp
@@ -1,88 +1,88 @@
 #include "../header/grammar.h"
 
 Grammar::Grammar() {
-  this->terminals = {};
-  this->startSymbol = "";
-  this->productions = {};
+    this->terminals = {};
+    this->startSymbol = "";
+    this->productions = {};
 }
 
 Grammar::Grammar(string startSymbol, set<string> terminals) {
-  this->startSymbol = startSymbol;
-  this->terminals = terminals;
+    this->startSymbol = startSymbol;
+    this->terminals = terminals;
 }
 
-void Grammar::addProduction(string A, vector<string> rhs){
-  productions[A].insert(rhs);
-  variables.insert(A);
+void Grammar::addProduction(string A, vector<string> rhs) {
+    productions[A].insert(rhs);
+    variables.insert(A);
 }
 
-void Grammar::removeProduction(string A, vector<string> rhs){
-  productions[A].erase(rhs);
+void Grammar::removeProduction(string A, vector<string> rhs) {
+    productions[A].erase(rhs);
 }
 
-set<vector<string>> Grammar::getProductions(string A){
-  auto it = productions.find(A);
-  if (it != productions.end()) {
-    return it->second;
-  }
-  return set<vector<string>>();
+set<vector<string> > Grammar::getProductions(string A) {
+    auto it = productions.find(A);
+    if (it != productions.end()) {
+        return it->second;
+    }
+    return set<vector<string> >();
 }
 
-void Grammar::print(ostream &out){
-  bool aux = true;
-  
-  out << "Terminais: <";
-  for (auto t : terminals) {
-    if (!aux) {
-      out << ", ";
-    }
-    aux = false;
-    out << t;
-  }
-  out << ">\n";
+void Grammar::print(ostream &out) {
+    bool aux = true;
 
-  out << "Produções: ";
-  auto startGrammar = productions.find(startSymbol);
-  if (startGrammar != productions.end()) {
-    out << startGrammar->first << " -> ";
-    aux = true;
-    for (auto t : startGrammar->second) {
-      if (!aux) {
-        out << " | ";
-      }
-      aux = false;
-      for (auto p : t) {
-        out << p;
-      }
+    out << "Terminais: <";
+    for (auto t: terminals) {
+        if (!aux) {
+            out << ", ";
+        }
+        aux = false;
+        out << t;
     }
-    out << "\n";
-  }
-  for (auto p : productions) {
-    if (p.first == startSymbol) {
-      continue;
-    }
-    out << "           " << p.first << " -> ";
-    aux = true;
-    for (auto ps : p.second) {
-      if (!aux) {
-        out << " | ";
-      }
-      aux = false;
-      for (auto pss : ps) {
-        out << pss;
-      }
-    }
-    out << "\n";
-  }
+    out << ">\n";
 
-  out << "Inicial:   " << this->startSymbol << "\n";
+    out << "Produções: ";
+    auto startGrammar = productions.find(startSymbol);
+    if (startGrammar != productions.end()) {
+        out << startGrammar->first << " -> ";
+        aux = true;
+        for (auto t: startGrammar->second) {
+            if (!aux) {
+                out << " | ";
+            }
+            aux = false;
+            for (auto p: t) {
+                out << p;
+            }
+        }
+        out << "\n";
+    }
+    for (auto p: productions) {
+        if (p.first == startSymbol) {
+            continue;
+        }
+        out << "\n           " << p.first << " -> ";
+        aux = true;
+        for (auto ps: p.second) {
+            if (!aux) {
+                out << " | ";
+            }
+            aux = false;
+            for (auto pss: ps) {
+                out << pss;
+            }
+        }
+        out << "\n";
+    }
+
+    out << "Inicial:   " << this->startSymbol << "\n";
 }
 
 Grammar Grammar::clone() const {
-  Grammar g2;
-  g2.terminals = this->terminals;
-  g2.startSymbol = this->startSymbol;
-  g2.productions = this->productions;
-  g2.variables = this->variables;
-  return g2;
+    Grammar g2;
+    g2.terminals = this->terminals;
+    g2.startSymbol = this->startSymbol;
+    g2.productions = this->productions;
+    g2.variables = this->variables;
+    return g2;
 }
--- src/grammarParser.cpp
+++ src/grammarParser.cpp
@@ -1,25 +1,33 @@
 #include "../header/grammarParser.h"
 
-const string RESET = "\033[0m";
-const string RED = "\033[31m";
-
 Grammar GrammarParser::parserFromFile(string filename) {
     Grammar grammar;
+    vector<string> v;
     ifstream file;
     file.open(filename);
 
     if (!file.is_open()) {
-        cout << RED << "Erro: não foi possível abrir o arquivo!" << RESET << endl;
+        cout << "\033[0m" << "Erro: não foi possível abrir o arquivo!" << "\033[31m" << endl;
         exit(EXIT_FAILURE);
     }
 
     string line;
     while (getline(file, line)) {
-        if (line.empty()) continue;
+        stringstream ss(line);
+        string ladoEsquerdo, seta;
+        v.clear();
 
-        size_t pos = line.find(" -> ");
-        if (pos != string::npos) {
-            grammar.addProduction(line.substr(0, pos), {line.substr(pos + 2)});
+        ss >> ladoEsquerdo;
+        ss >> seta;
+
+        if (seta == "->") {
+            char c;
+            while (ss.get(c)) {
+                if (c != ' ') {
+                    v.push_back(string(1, c));
+                }
+            }
+            grammar.addProduction(ladoEsquerdo, v);
         }
     }
     return grammar;
